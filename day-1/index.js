d1b0fd2683e6b2eb76e92b6457a361ab1b57e75d
siddharthugharejiya <sidhdharthugharejiya8182@gmail.com>
2024-09-13 11:16:12 +0530

    .

---
+++ day-1/index.js
@@ -0,0 +1,8 @@
+const {sum,sub,mul,div} = require("./function")
+const a=Number(process.argv[2])  
+const b=Number(process.argv[3])
+console.log(sum(a,b))
+console.log(sub(a,b))
+console.log(mul(a,b))
+console.log(div(a,b))
+      
